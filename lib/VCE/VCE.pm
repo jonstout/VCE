94721ee31b3737c38813923e41ff370281566d7f
AJ Ragusa <ajragusa@gmail.com>
2016-11-07 20:02:49 +0000

    adding some new files

--- lib/VCE/VCE.pm
+++ lib/VCE/VCE.pm
@@ -13,6 +13,7 @@ use GRNOC::RabbitMQ;
 has config_file => (is => 'rwp');
 has config => (is => 'rwp');
 has logger => (is => 'rwp');
+has access => (is => 'rwp');
 
 sub BUILD{
     my ($self) = @_;
@@ -20,21 +21,54 @@ sub BUILD{
     my $logger = GRNOC::Log->get_logger("VCE");
     $self->_set_logger($logger);
     
-    $self->process_config();
+    $self->_process_config();
 
-    
+    $self->_set_access( VCE::Access->new( config => $self->config,
+					  logger => $self->logger ));
     
     return $self;
 }
 
-sub process_config{
+sub _process_config{
     my $self = shift;
 
     my $config = GRNOC::Config->new( config_file => $self->config_file);
     
-    
-}
+    my %workgroups;
+    my %users;
 
+    my $wgs = $config->get('/accessPolicy/workgroup');
+    foreach my $workgroup (@$wgs){
+	$self->logger->debug("Processing workgroup: " . Data::Dumper::Dumper($workgroup));
+	my $grp = {};
+	$grp->{'name'} = $workgroup->{'name'};
+	$grp->{'description'} = $workgroup->{'description'};
+	$grp->{'user'} = $workgroup->{'user'};
+	$workgroups{$grp->{'name'}} = $grp;
+	foreach my $user (@{$grp->{'user'}}){
+	    if(!defined($users{$user})){
+		$users{$user} = ();
+	    }
+	    push(@{$users{$user}},$grp->{'name'});
+	}
+    }
+    
+    my $cfg = {};
+    $cfg->{'users'} = \%users;
+    $cfg->{'workgroups'} = \%workgroups;
+    
+    my %switches;
+    my $switches = $config->get('/accessPolicy/switch');
+    foreach my $switch (@$switches){
+	$self->logger->debug("Processing switch: " . Data::Dumper::Dumper($switch));
+	$switches{$switch->{'name'}} = $switch;
+    }
+
+    $cfg->{'switches'} = \%switches;
+    $self->_set_config($cfg);
+    $self->logger->debug("Config: " . Data::Dumper::Dumper($self->config));
+
+}
 
 
 1;
