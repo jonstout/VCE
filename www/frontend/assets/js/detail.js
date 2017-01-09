058751436a82d285984917834422290094021bf2
Jonathan Stout <jonstout@grnoc.iu.edu>
2017-01-09 19:10:37 +0000

    trying new event func for dropdown

--- www/frontend/assets/js/detail.js
+++ www/frontend/assets/js/detail.js
@@ -119,7 +119,7 @@ function navigateOnSelect(e) {
 
 function loadVlans() {
     var create = document.getElementById('vlan_command');
-    create.addEventListener("select", navigateOnSelect, false);
+    create.addEventListener("change", navigateOnSelect, false);
     
     var cookie = Cookies.getJSON('vce');
     var name = cookie.switch;
