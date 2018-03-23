7fec3000aa9c5059f83b61db630d9cfd2ece20c2
Jeffrey <jeffrey.peter1@gmail.com>
2018-03-23 16:45:07 +0000

    Replaced Vlan UUID with Vlan ID Fixes 148.

--- www/frontend/assets/js/detail.js
+++ www/frontend/assets/js/detail.js
@@ -193,8 +193,8 @@ function configureVLANButtons(e) {
         var url = baseUrl + 'provisioning.cgi?method=delete_vlan';
         url += '&workgroup=' + workgroup;
         url += '&vlan_id=' + vlanId;
-
-        if (!confirm(`This action will delete VLAN ${vlanId}.\n\n Do you wish to continue?`)) {
+	var vlanName= $('#'+vlanId).contents().first().text();
+        if (!confirm(`This action will delete VLAN ${vlanName}.\n\n Do you wish to continue?`)) {
             return 1;
         }
 
