fbca11ea4abfc7106fee049310d4576425166a5b
Jonathan Stout <jonstout@globalnoc.iu.edu>
2018-07-24 14:10:54 +0000

    stashing my changes on this branch

--- lib/VCE/Database/VLAN.pm
+++ lib/VCE/Database/VLAN.pm
@@ -6,40 +6,37 @@ use warnings;
 use Exporter;
 
 our @ISA = qw( Exporter );
-our @EXPORT = qw( add_vlan_acl get_vlan_acls );
+our @EXPORT = qw( add_vlan get_vlans );
 
 
-sub add_vlan_acl {
-    my ( $self, $interface_id, $workgroup_id, $low, $high ) = @_;
+sub add_vlan {
+    my ( $self ) = @_;
 
-    $self->{log}->debug("get_vlan_acls($interface_id, $workgroup_id, $low, $high)");
+    $self->{log}->debug("add_vlan()");
 
     my $q = $self->{conn}->prepare(
-        "insert into interface_workgroup_vlan_acl
-         (interface_id, workgroup_id, low, high)
-         values (?, ?, ?, ?)"
+        "select * from vlan"
     );
-    $q->execute($interface_id, $workgroup_id, $low, $high);
+    $q->execute();
 
-    return $self->{conn}->last_insert_id("", "", "interface_workgroup_vlan_acl", "");
+    return $self->{conn}->last_insert_id("", "", "vlan", "");
 }
 
-sub get_vlan_acls {
-    my ( $self, $interface_id, $workgroup_id ) = @_;
+sub get_vlans {
+    my ( $self, $workgroup_id ) = @_;
 
-    $self->{log}->debug("get_vlan_acls($self->{conn}, $interface_id, $workgroup_id)");
+    $self->{log}->debug("get_vlans($self->{conn}, $workgroup_id)");
 
     my $q = $self->{conn}->prepare(
-        "select * from interface_workgroup_vlan_acl as iwva
-         where iwva.workgroup_id=? and iwva.interface_id=?"
+        "select * from vlan"
     );
-    $q->execute($workgroup_id, $interface_id);
+    $q->execute();
 
-    my $acls = $q->fetchall_arrayref({});
+    my $vlans = $q->fetchall_arrayref({});
     my $result = [];
 
-    foreach my $acl (@$acls) {
-        push @$result, { high => $acl->{high}, low  => $acl->{low} };
+    foreach my $vlan (@$vlans) {
+        push @$result, $vlan;
     }
 
     return $result;
