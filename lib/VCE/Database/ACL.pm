67bbbb8d70794cf1ba8f7ee17088c35d50ad63e4
Akash Sheth <akashbsheth@gmail.com>
2018-08-22 22:15:46 +0000

    ACL: Add, modify and delete - Dev:done, Testing:remaining

--- lib/VCE/Database/ACL.pm
+++ lib/VCE/Database/ACL.pm
@@ -2,28 +2,41 @@ package VCE::Database::ACL;
 
 use strict;
 use warnings;
-
+use Data::Dumper;
 use Exporter;
 
 our @ISA = qw( Exporter );
-our @EXPORT = qw( add_acl get_acls );
+our @EXPORT = qw( add_acl get_acls modify_acl delete_acl);
 
 
 =head2 add_acl
 =cut
 sub add_acl {
-    my ( $self, $workgroup_id, $interface_id, $low, $high ) = @_;
+    my ( $self, $interface_id, $workgroup_id, $low, $high ) = @_;
 
     $self->{log}->debug("add_acl($workgroup_id, $interface_id, $low, $high)");
 
-    my $q = $self->{conn}->prepare(
-        "insert into acl
-         (interface_id, workgroup_id, low, high)
-         values (?, ?, ?, ?)"
-    );
-    $q->execute($interface_id, $workgroup_id, $low, $high);
+    eval {
+        my $q = $self->{conn}->prepare(
+            "insert into acl
+            (interface_id, workgroup_id, low, high)
+            values (?, ?, ?, ?)"
+        );
+        $q->execute($interface_id, $workgroup_id, $low, $high);
 
-    return $self->{conn}->last_insert_id("", "", "interface_workgroup_acl", "");
+    };
+
+
+    if ($@) {
+        # $self->{log}->error("$@");
+        warn Dumper($@);
+        return (undef,$@)
+    }
+
+    my $id = $self->{conn}->last_insert_id("", "", "acl", "");
+    return ($id, undef);
+
+    # return $self->{conn}->last_insert_id("", "", "interface_workgroup_acl", "");
 }
 
 =head2 get_acls
@@ -35,7 +48,7 @@ sub get_acls {
 
     my $q = $self->{conn}->prepare(
         "select * from acl
-         where acl.workgroup_id=? and acl.interface_id=?"
+        where acl.workgroup_id=? and acl.interface_id=?"
     );
     $q->execute($workgroup_id, $interface_id);
 
@@ -49,4 +62,74 @@ sub get_acls {
     return $result;
 }
 
+=head2 modify_acl
+
+
+=cut
+sub modify_acl {
+    my $self   = shift;
+    my %params = @_;
+    return if (!defined $params{id});
+
+    $self->{log}->debug("modify_acl($params{id}, ...)");
+
+    my $keys = [];
+    my $args = [];
+
+    if (defined $params{low}) {
+        push @$keys, 'low=?';
+        push @$args, $params{low};
+    }
+    if (defined $params{high}) {
+        push @$keys, 'high=?';
+        push @$args, $params{high};
+    }
+
+    my $values = join(', ', @$keys);
+    push @$args, $params{id};
+    my $result;
+    eval {
+        my $q = $self->{conn}->prepare(
+            "UPDATE acl SET $values WHERE id=?"
+        );
+
+        $result = $q->execute(@$args);
+    };
+    if ($@) {
+        
+        warn Dumper($@);
+        $self->{log}->error("$@");
+        return 0;
+    }
+    return $result;
+}
+
+=head2 delete_acl
+
+=cut
+sub delete_acl {
+    my $self = shift;
+    my $acl_id = shift;
+
+    $self->{log}->debug("Calling delete_acl");
+
+    if (!defined $acl_id) {
+        $self->{log}->error("No acl id specified");
+        return;
+    }
+
+    eval {
+        my $query = $self->{conn}->prepare(
+            'DELETE FROM acl WHERE id=?'
+        );
+        $query->execute($acl_id);
+    };
+    if ($@) {
+        warn Dumper($@);
+        $self->{log}->error("$@");
+        return 0;
+    }
+
+    return 1;
+}
 return 1;
